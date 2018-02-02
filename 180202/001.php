ae23bfa8348c71b839e52aa49cbed30576b5881a
heriircloud <36062636+heriircloud@users.noreply.github.com>
2018-02-02 13:32:09 +0700

    Update 001.php

--- 180202/001.php
+++ 180202/001.php
@@ -1,7 +1,33 @@
 <?php
+
+//function tambah
+function tambah($int1,$int2){
+  return $int1+$int2;
+}
+//function kurang
+function kurang($int1,$int2){
+  return $int1-$int2;
+}
+//function kali
+function kali($int1,$int2){
+  return $int1*$int2;
+}
+//function bagi
+function bagi($int1,$int2){
+  return $int1/$int2;
+}
+
 $a = 1;
 $b = 2;
 $c = $a + $b;
+
 echo $a;
 echo $b;
 echo $c;
+
+echo tambah($a,$b);
+echo kurang($a,$b);
+echo kali($a,$b);
+echo bagi($a,$b);
+
+echo tambah(10,30);
